95c52702195c5e948eb63f4e143b17b4e9f79798
bayda <yemcoder@gmail.com>
2025-01-11 08:55:52 +0100

    change start url

--- main.js
+++ main.js
@@ -4,39 +4,3 @@ OneSignalDeferred.push(async function(OneSignal) {
         appId: "47cd6975-ca92-4101-8391-3b2afc31508d",
     });
 });
-
-console.log(OneSignalDeferred);
-
-
-/*
-
-
-window.OneSignal = window.OneSignal || [];
-
-
-OneSignal.push(function() {
-    OneSignal.init({
-        appId: "47cd6975-ca92-4101-8391-3b2afc31508d",
-        // safari_web_id: 'ВАШ_SAFARI_WEB_ID', // Если применимо
-        notifyButton: {
-            enable: true, // Показывать кнопку уведомлений
-        },
-        allowLocalhostAsSecureOrigin: true, // Разрешить localhost для разработки
-    });
-});
-
-// Кнопка подписки
-const subscribeButton = document.getElementById('subscribe');
-
-console.log(subscribeButton);
-
-
-subscribeButton.addEventListener('click', () => {
-    console.log('subscribeButton click...');
-    OneSignal.push(function() {
-        OneSignal.showSlidedownPrompt();
-        console.log('showSlidedownPrompt ...');
-    });
-});
-*/
-
